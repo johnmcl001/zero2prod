02597da8c7fd0bf1b9cc8fbf9338064cb8dddd9a
john.mcloughlin <john.mcloughlin.03@gmail.com>
2023-05-18 07:28:07 +0100

    Dockerfile

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,17 @@
+use std::path::PathBuf;
+
 use secrecy::{ExposeSecret, Secret};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -16,12 +24,62 @@ pub struct DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory.");
+    let config_dir = base_path.join("configuration");
+    let base_config = get_config_from_file(config_dir.clone(), "base");
+
+    let env: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let env_config = get_config_from_file(config_dir, env.as_str());
+
     config::Config::builder()
-        .set_default("default", "1")?
-        .add_source(config::File::with_name("configuration"))
-        .set_override("override", "1")?
+        .add_source(config::Config::default())
+        .add_source(base_config.required(true))
+        .add_source(env_config.required(true))
         .build()?
         .try_deserialize()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
+
+fn get_config_from_file(
+    dir: PathBuf,
+    file_name: &str,
+) -> config::File<config::FileSourceFile, config::FileFormat> {
+    dir.join(file_name)
+        .as_path()
+        .to_str()
+        .map(config::File::with_name)
+        .expect("Base config file not found")
 }
 
 impl DatabaseSettings {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use secrecy::ExposeSecret;
 use std::net::TcpListener;
 
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,10 +12,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let db_pool = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let db_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to DB.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     TcpListener::bind(address)
         .map(|addr| run(addr, db_pool))??
         .await
